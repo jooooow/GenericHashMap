f80192677a57c287d981bdba4cf1ec00109dc000
jooooow <jooooow@outlook.com>
2017-08-30 23:37:33 +0800

    general commit

--- test/Main.cpp
+++ test/Main.cpp
@@ -17,13 +17,13 @@ int main()
 #ifdef DEBUG
 	enableMemLeakCheck();
 #endif
-	auto *map = new HashMap<char,char>();
-	map->insert('1', 'h');
-	map->insert('2', 'f');
-	map->insert('7', 'y');
-	std::cout << map->getValue('1') << '\n';
-	std::cout << map->getValue('2') << '\n';
-	std::cout << map->getValue('7') << '\n';
+	auto *map = new HashMap<const char*,char>();
+	map->insert("123", 'h');
+	map->insert("456", 'f');
+	map->insert("789", 'y');
+	std::cout << map->getValue("123") << '\n';
+	std::cout << map->getValue("456") << '\n';
+	std::cout << map->getValue("789") << '\n';
 	delete map;
 	map = nullptr;
 	getchar();
