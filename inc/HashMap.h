d4fb97849da7e67f6d583580d073bc23af89ad56
jooooow <jooooow@outlook.com>
2017-08-31 22:18:12 +0800

    add remove function, fix bug
    and type optimize

--- inc/HashMap.h
+++ inc/HashMap.h
@@ -14,38 +14,57 @@ public:
 		Node* next = nullptr;
 	};
 	typedef std::vector<Node*>	NodeVector;
+	typedef const Key&			constKeyRef;
+	typedef const Value&		constValueRef;
+	typedef HashMap&&			RhashMap;
+	typedef const HashMap&		constMapRef;
 private:
-	int size;
-	NodeVector *vec;
+	int size;											/*size of the vector*/
+	NodeVector *vec;									/*pointer of vector*/
 public:
-	HashMap(int _size = 100);
-	~HashMap();
-	bool insert(Key key, Value value);
-	bool remove(Key key);
-	bool isBucketEmpty(int index);
-	bool find(int index, Value value);
-	Value getValue(Key key);
+	HashMap(int _size = 100);							/*default constructor*/
+	HashMap(constMapRef map);							/*copy constructor*/
+	HashMap(RhashMap map);								/*move constructor*/
+	~HashMap();											/*destructor*/
+	bool insert(constKeyRef key, constValueRef value);	/*insert a pair*/
+	bool remove(constKeyRef key);						/*remove a pair*/
+	bool find(constKeyRef key, constValueRef value);	/*return if the pair is in map*/
+	Value getValue(constKeyRef key);					/*use key to get value*/
 private:
-	//int hash(Key key);
-	int hash(const int& key);
-	int hash(const char& key);
-	int hash(const char* key);
-	int hash(const unsigned int& key);
-	int hash(const std::string& key);
-	void deleteAll();
+	bool isBucketEmpty(int index);						/*return if the bucket is empty*/
+	void deleteAll();									/*delete all the nodes*/
+private:
+	int hash(const int& key);							/*use int to get hash value*/
+	int hash(const char& key);							/*use char to get hash value*/
+	int hash(const char* key);							/*use char* to get hash value*/
+	int hash(const unsigned int& key);					/*use unsigned int to get hash value*/
+	int hash(const std::string& key);					/*use std::string to get hash value*/
 };
 
 template<typename Key, typename Value>
 HashMap<Key, Value>::HashMap(int _size) : size(_size)
 {
-	vec = new std::vector<Node*>();
-	for (int i = 0; i < size; i++)
+	vec = new std::vector<Node*>();						/*allocate memory*/
+	for (int i = 0; i < size; i++)						/*initilizate vector*/
 	{
 		Node* p = nullptr;
 		vec->push_back(p);
 	}
 }
+template<typename Key, typename Value>
+HashMap<Key, Value>::HashMap(RhashMap map)
+{
+	size = map.size;			
+	vec = map.vec;										/*move vector*/
+	map.vec = nullptr;
+}
+template<typename Key, typename Value>
+HashMap<Key, Value>::HashMap(constMapRef map)
+{
+	HashMap(map.size);									
 
+	//TODO
+}
 template<typename Key, typename Value>
 HashMap<Key, Value>::~HashMap()
 {
@@ -54,7 +73,7 @@ HashMap<Key, Value>::~HashMap()
 }
 
 template<typename Key, typename Value>
-bool HashMap<Key, Value>::insert(Key key, Value value)
+bool HashMap<Key, Value>::insert(constKeyRef key, constValueRef value)
 {
 	int index = hash(key);
 	if (isBucketEmpty(index))
@@ -67,15 +86,16 @@ bool HashMap<Key, Value>::insert(Key key, Value value)
 		(*p)->key = key;
 
 		*****************************/
-		vec->at(index) = new Node();
+		vec->at(index) = new Node();						
 		auto p = vec->at(index);
 		p->value = value;
 		p->key = key;
+		p->next = nullptr;
 		return true;
 	}
 	else
 	{
-		if (!find(index, value))
+		if (!find(key, value))
 		{
 			auto p = vec->at(index);
 			while (p != nullptr)
@@ -92,28 +112,46 @@ bool HashMap<Key, Value>::insert(Key key, Value value)
 	}
 	return true;
 }
-
 template<typename Key, typename Value>
-bool HashMap<Key, Value>::remove(Key key)
+bool HashMap<Key, Value>::remove(constKeyRef key)
 {
-	//TODO
-
-	return true;
+	int index = hash(key);
+	if (isBucketEmpty(index))
+	{
+		return false;
+	}
+	auto current = &vec->at(index);
+	Node* temp;
+	while ((*current) != nullptr)
+	{
+		if ((*current)->key == key)
+		{
+			temp = (*current)->next;
+			delete *current;
+			*current = temp;
+			return true;
+		}
+		*current = (*current)->next;
+	}
+	return false;
 }
-
 template<typename Key, typename Value>
 bool HashMap<Key, Value>::isBucketEmpty(int index)
 {
 	auto p = vec->at(index);
 	if (p == nullptr)
+	{
 		return true;
+	}
 	else
+	{
 		return false;
+	}
 }
-
 template<typename Key, typename Value>
-bool HashMap<Key, Value>::find(int index, Value value)
+bool HashMap<Key, Value>::find(constKeyRef key, constValueRef value)
 {
+	int index = hash(key);
 	Node* temp = vec->at(index);
 	while (temp != nullptr)
 	{
@@ -123,9 +161,8 @@ bool HashMap<Key, Value>::find(int index, Value value)
 	}
 	return false;
 }
-
 template<typename Key, typename Value>
-Value HashMap<Key, Value>::getValue(Key key)
+Value HashMap<Key, Value>::getValue(constKeyRef key)
 {
 	int index = hash(key);
 	if (isBucketEmpty(index))
@@ -144,22 +181,9 @@ Value HashMap<Key, Value>::getValue(Key key)
 	}
 }
 
-//template<typename Key, typename Value>
-//int HashMap<Key, Value>::hash(Key key)
-//{
-//	if (typeid(key) == typeid(int))
-//	{
-//		hash(key);
-//	}
-//	else if (typeid(key) == typeid(char))
-//	{
-//		return (int)key % size;
-//	}
-//	else
-//	{
-//		return 0;
-//	}
-//}
+/********************
+	hash functions
+*********************/
 template<typename Key, typename Value>
 int HashMap<Key, Value>::hash(const int& key)
 {
@@ -189,7 +213,6 @@ int HashMap<Key, Value>::hash(const std::string& key)
 {
 	return 0;
 }
-
 template<typename Key, typename Value>
 void HashMap<Key, Value>::deleteAll()
 {
@@ -201,6 +224,8 @@ void HashMap<Key, Value>::deleteAll()
 		}
 		else
 		{
+			/***** old function *****
+
 			auto current = vec->at(i);
 			Node* temp;
 			while (current != nullptr)
@@ -208,6 +233,17 @@ void HashMap<Key, Value>::deleteAll()
 				temp = current->next;
 				delete current;
 				current = temp;
+			}
+
+			*************************/
+
+			auto current = &vec->at(i);
+			Node* temp;
+			while ((*current) != nullptr)
+			{
+					temp = (*current)->next;
+					delete *current;
+					*current = temp;
 			}
 		}
 	}
